164a1b24752bce43d9ac20d4a008e3d4f75cb027
Karandeep <softwaredevkaran@gmail.com>
2026-04-03 14:36:39 +0530

    Update server/lib/utils.js

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -3,7 +3,9 @@ import jwt from "jsonwebtoken";
 // Function to generate a token
 export const generateToken = (userId) => {
 
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET);
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+    });
     return token;
     
 }

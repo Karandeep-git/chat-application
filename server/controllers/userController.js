53a655388533048316aeac2ad2cef8bfd5beede5
Karandeep <softwaredevkaran@gmail.com>
2026-04-03 14:31:00 +0530

    added bcrypt importation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.js";
+import bcrypt from "bcryptjs";
 
 // Signup new user
 export const signup = async (req, res) => {
